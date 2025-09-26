fe69483669c42908e4583bd464fa834a75d1b500
Ngwanchang <nchanjiemilia1@gmail.com>
2025-09-26 07:15:42 +0200

    learning how to build an advanced portfolio website using next.js, tailwind and three.js

---
+++ src/app/about/page.jsx
@@ -0,0 +1,33 @@
+'use client';
+
+import Image from 'next/image';
+import Navigation from '@/components/navigation';
+import Rendermodel from '@/components/Rendermodel';
+import WizardModel from '@/components/models/wizard';
+export default function AboutPage() {
+
+  return (
+    <main className="relative min-h-screen w-full">
+       <div className="absolute inset-0">
+         <Image 
+           src="/background/about-background.png" 
+           alt="background-image" 
+           fill
+           priority
+           className="object-cover object-center opacity-25"
+         />
+       </div>
+      <div className="relative z-10 mx-auto max-w-3xl px-4 py-12 flex flex-col items-center text-center gap-8">
+        <h1 className="text-3xl font-semibold">about page</h1>
+       
+        <Navigation />
+        <div className="w-full h-[420px]">
+          <Rendermodel className="w-full h-[420px] z-10">
+            <WizardModel />
+          </Rendermodel>
+        </div>
+      </div>
+      </main>
+  )
+  
+}
